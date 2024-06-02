80087a12e518ad6dea969c888a85345cf056160e
Sadiqovilkin <sadiqovilkin068@gmail.com>
2024-06-02 16:09:06 +0400

    Router pull origin ramal

--- site/src/components/Layout/Header/Header.jsx
+++ site/src/components/Layout/Header/Header.jsx
@@ -1,29 +1,28 @@
 import React, { useState } from 'react'
 
 const Header = () => {
-    const [nav , setNav]= useState("d-none")
-  return (
-    <>
-    
-    <header>
-        header
-        <button onClick={()=>{setNav("d-block")}}>click</button>
-        
-    </header>
-    <nav className={nav}>
-    <div className="container-fluid">
-        <div className="row">
-            <div className="col-lg-6"></div>
-            <div className="col-lg-6">
-                close
-                nav_items 
-                nav_copyright
-            </div>
-        </div>
-    </div>
-    </nav>
-    </>
-  )
+    const [nav, setNav] = useState("d-none")
+    return (
+        <>
+
+            <header>
+                header
+                <button onClick={() => { setNav("d-block") }}>click</button>
+            </header>
+            <nav className={nav}>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-lg-6"></div>
+                        <div className="col-lg-6">
+                            close
+                            nav_items
+                            nav_copyright
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Header
---
+++ site/src/routes/index.jsx
@@ -0,0 +1,60 @@
+import AdminLogin from "../pages/Admin/AdminLogin/AdminLogin";
+import AdminRoot from "../pages/Admin/AdminRoot";
+import About from "../pages/Client/About/About";
+import Login from "../pages/Client/ClientLogin/Login";
+import ClientRoot from "../pages/Client/ClientRoot";
+import Collections from "../pages/Client/Collections/Collections";
+import Home from "../pages/Client/Home/Home";
+import Register from "../pages/Client/Register/Register";
+
+export const ROUTES = [
+
+
+    // Admin Root
+
+    {
+
+        path: "admin",
+        element: <AdminRoot />,
+        children: [
+
+            {
+                path: "login",
+                element: <AdminLogin />
+
+            }
+        ]
+
+
+    },
+
+    // Client Root
+    {
+        path: '/',
+        element: <ClientRoot />,
+        children: [
+
+            {
+                index: true,
+                elment: <Home />
+            },
+            {
+                path: "about",
+                element: <About />
+            },
+            {
+                path: "collections",
+                element: <Collections />
+            },
+            {
+                path: "register",
+                element: <Register />
+            },
+            {
+                path: "login",
+                element: <Login />
+            }
+        ]
+
+    }
+]
